83d0b012496080564b59b91de32de9fa2fb70f31
Arnab saha <68840187+thearnabsaha@users.noreply.github.com>
2024-04-14 18:41:37 +0000

    Dom

---
+++ 01_basics/next.js
@@ -0,0 +1,28 @@
+// document.getElementById("heading") //to get the the element by it's id
+// document.getElementById("heading").id //to get the id
+// document.getElementById("heading").class // it will show undefined check classname
+// document.getElementById("heading").className //to get the class
+// document.getElementById("heading").getAttribute("id") // get value of attribute id 
+// document.getElementById("heading").getAttribute("class") // get value of attribute class
+// document.getElementById("heading").setAttribute("class","test") //set value of a attribute 
+// document.getElementById("heading").setAttribute("class","test heading") //set 2 value of a attribute at the same time
+// const haeding=document.getElementById("heading") //save it to variable
+// haeding.style.backgroundColor="green" //this is how you can change the css
+// haeding.style.padding="15px" //this is how you can change the css
+// haeding.textContent // it will show all the contents even if it is hidden by css properties
+// haeding.innerHTML // this will give the whole html tag
+// haeding.innerText // it won't show all the contents if it is hidden by css properties
+// document.getElementsByClassName("heading") //to get the element by it's class
+// document.querySelector("h1") // to get the element by tag in queryselector
+// document.querySelector(".heading") // to get the element by classname in queryselctor
+// document.querySelector("#heading") // to get the element by id in queryselctor
+// document.querySelector("input[type=password]") // to get the element by tag and attribute in queryselctor
+// document.querySelectorAll(".heading")  // to get the element by classname in queryselctorall
+// document.querySelectorAll("li") // top select all the li
+// document.querySelectorAll("li")[0].style.color="green" //to change the first li's color
+// document.querySelectorAll("li").forEach((i)=>i.style.color="green") //to change all the li's color
+
+// let lists= document.getElementsByClassName("myli") // we get as text-nodes what hasn't foreach property so we change it to array
+// lists=Array.from(lists)
+// lists.forEach((i)=>i.style.color="green")
+
